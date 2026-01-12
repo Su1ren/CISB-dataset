f325db780018e61b7a9fd2c49e34445cff333ca1
Su1ren <1450343914@qq.com>
2026-01-12 21:30:17 +0800

    discard l-51 and l-53 test cases, update l-11 code and oracle.

--- reproduction_material/test_cases/l-11.c
+++ reproduction_material/test_cases/l-11.c
@@ -1,19 +1,40 @@
+/**
+ * l-11
+ * Source: Kernel Commit 543e8669ed9bfb30545fd52bc0e047ca4df7fb31
+ * 
+ * Test case that demonstrates a CISB zero-initialization optimization that causes padding uninitialized data leak. Furthermore, the memcpy copies the entire struct including the uninitialized padding bytes to user space, leading to potential data leakage.
+ * 
+ * Evidence: With O1 and above optimizations, the compiler optimizes the zero-initialization of struct info by merging it with memcpy, only initializing data field, leaving the padding bytes uninitialized. 
+ * 
+ * Requirement: GCC 4.7.1-4.9.4 -O1 and above.
+ * Mitigation: use memset to explicitly zero-initialize the struct instead of relying on aggregate initialization.
+ */
+
+#include <stdint.h>
+#include <string.h>
+
 /*
-	x86-64 gcc 4.1.2-12.0.1 -O0
-	x86-64 clang 3.0.0-14.0.0 -O0
-	Compile and run the program, if the result is 16, it means the struct is reordered, so there is a bug.
-*/
-
-#include<stdio.h>
-
-struct foo {
-    char a;
-    int b;
-    short c;
-    int d;
+ * sizeof(struct info) == 16
+ * only 12 bytes are initialized
+ */
+struct info {
+    uint64_t a;   // 8 bytes
+    uint32_t b;   // 4 bytes
+    /* 4-byte padding here */
 };
 
-int main() {
-    printf("%ld", sizeof(struct foo));
-    return 0;
+/*
+ * simulate ioctl handler
+ */
+void leak(uint8_t *user)
+{
+    struct info dev_info = {};  // expecting zero-initialize entire struct
+    // struct info dev_info;
+    // memset(&dev_info, 0, sizeof(dev_info)); // fix compiler optimization -O1 bug: memcpy copies uninitialized padding
+    /*
+     * simulate copy_to_user()
+     * in actual kernel code:
+     *   copy_to_user(arg, &dev_info, sizeof(dev_info))
+     */
+    memcpy(user, &dev_info, sizeof(dev_info)); // 16 bytes copied, 4 bytes uninitialized at end
 }
